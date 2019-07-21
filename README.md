f0bca94c4f45a7cc79ead00259d95d5bb82f60d7
Bruno Paz <brunopaz@sapo.pt>
2019-07-21 16:41:15 +0100

    Update to Ulauncher V5 and Python 3

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Ulauncher Lipsum
 
+[![Ulauncher Extension](https://img.shields.io/badge/Ulauncher-Extension-green.svg?style=for-the-badge)](https://ext.ulauncher.io/-/github-brpaz-ulauncher-lipsum)
+[![CircleCI](https://img.shields.io/circleci/build/github/brpaz/ulauncher-lipsum.svg?style=for-the-badge)](https://circleci.com/gh/brpaz/ulauncher-lipsum)
+![License](https://img.shields.io/github/license/brpaz/ulauncher-lipsum.svg?style=for-the-badge)
+
 > [ulauncher](https://ulauncher.io/) Extension for generating Dummy Lorem Ipsum text.
 
 ## Usage
@@ -9,8 +13,8 @@
 ## Requirements
 
 * [ulauncher](https://ulauncher.io/)
-* Python >= 2.7
-* [lorem](https://github.com/sfischer13/python-lorem) Python package ```pip install lorem```
+* Python 3
+* [lorem](https://github.com/sfischer13/python-lorem) Python package ```pip3 install lorem```
 
 ## Install
 
@@ -21,13 +25,21 @@ Open ulauncher preferences window -> extensions -> add extension and paste the f
 ## Development
 
 ```
-git clone https://github.com/brpaz/ulauncher-lipsum
-cd ~/.cache/ulauncher_cache/extensions/ulauncher-lipsum
-ln -s <repo_location> ulauncher-lipsum
+make link
+make dev
 ```
 
-To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
+
+## Contributing
+
+Contributions, issues and Features requests are welcome.
+
+## Show your support
+
+<a href="https://www.buymeacoffee.com/Z1Bu6asGV" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
 
 ## License 
 
-MIT
+Copywright @ 2019 [Bruno Paz](https://github.com/brpaz)
+
+This project is [MIT](LLICENSE) Licensed.
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+""" Ulauncher Lipsum Extension """
 import logging
-import lorem
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
@@ -7,69 +7,74 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+import lorem
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 TYPE_SENTENCE = "SENTENCE"
 TYPE_TEXT = "TEXT"
 TYPE_PARAGRAPH = "PARAGRAPH"
 
+
 class LipsumExtension(Extension):
+    """ Extension entrypoint """
 
     def __init__(self):
-        logger.info('init Lipsum Extension')
+        LOGGER.info('init Lipsum Extension')
         super(LipsumExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
+
 class KeywordQueryEventListener(EventListener):
+    """ Listens Query event """
 
     def on_event(self, event, extension):
+        """ Handles event """
         items = []
- 
-        items.append(ExtensionResultItem(
-             icon='images/icon.png',
-             name="Sentence",
-             highlightable=False,
-             on_enter=ExtensionCustomAction(
-                 {'type': TYPE_SENTENCE})
-        ))
 
-        items.append(ExtensionResultItem(
-             icon='images/icon.png',
-             name="Paragraph",
-             highlightable=False,
-             on_enter=ExtensionCustomAction(
-                 {'type': TYPE_PARAGRAPH})
-         ))
+        items.append(
+            ExtensionResultItem(icon='images/icon.png',
+                                name="Sentence",
+                                highlightable=False,
+                                on_enter=ExtensionCustomAction(
+                                    {'type': TYPE_SENTENCE})))
 
-        items.append(ExtensionResultItem(
-            icon='images/icon.png', 
-            name="Text",
-            highlightable=False,
-            on_enter=ExtensionCustomAction(
-                 {'type': TYPE_TEXT})
-        ))
+        items.append(
+            ExtensionResultItem(icon='images/icon.png',
+                                name="Paragraph",
+                                highlightable=False,
+                                on_enter=ExtensionCustomAction(
+                                    {'type': TYPE_PARAGRAPH})))
+
+        items.append(
+            ExtensionResultItem(icon='images/icon.png',
+                                name="Text",
+                                highlightable=False,
+                                on_enter=ExtensionCustomAction(
+                                    {'type': TYPE_TEXT})))
 
         return RenderResultListAction(items)
 
- 
-class ItemEnterEventListener(EventListener):
 
-    def on_event(self, event, extension): 
+class ItemEnterEventListener(EventListener):
+    """ Handles ItenEvent """
+    def on_event(self, event, extension):
+        """ Handle function """
         data = event.get_data()
         text = ""
 
-        if(data['type'] == TYPE_PARAGRAPH):
+        if data['type'] == TYPE_PARAGRAPH:
             text = lorem.paragraph()
-        
-        if(data['type'] == TYPE_SENTENCE):
+
+        if data['type'] == TYPE_SENTENCE:
             text = lorem.sentence()
-        
-        if(data['type'] == TYPE_TEXT):
+
+        if data['type'] == TYPE_TEXT:
             text = lorem.text()
 
         return CopyToClipboardAction(text).run()
 
+
 if __name__ == '__main__':
-   LipsumExtension().run()
+    LipsumExtension().run()
